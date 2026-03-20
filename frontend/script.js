917f58e091ddb14ec22ceb7b34ec927d90f0d897
vaishnavishh <hheeruu786@gmail.com>
2026-03-20 21:28:56 +0530

    Linked live Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -8,7 +8,7 @@ document.getElementById('predictForm').addEventListener('submit', async function
 
     resultDiv.innerText = "Processing...";
 
-    const response = await fetch('http://127.0.0.1:5000/predict', {
+    const response = await fetch('https://minor-project-ya5t.onrender.com/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
